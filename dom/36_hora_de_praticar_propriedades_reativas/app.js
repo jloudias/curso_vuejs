73c505841bba03990ae1aff56282cbe725762d4b
Jorge Dias <jloudias@gmail.com>
2022-08-31 21:08:41 -0300

    Desafio 2

---
+++ dom/36_hora_de_praticar_propriedades_reativas/app.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resultado(){
+            return this.valor == 37?"Valor igual":"Valor diferente"
+
+        }
+    },
+    watch: {
+        resultado(){
+            setTimeout(() => {
+                this.valor = 0
+            }, 3000)
+                
+        }
+
+    },
+   
+
+});
